cb4fa377844fcf9cb33a5b2a975819a4f67a1cde
Mary Eby <m.eby@sms.ed.ac.uk>
2022-01-21 15:59:04 +0000

    Refactor according to PEP8 and numpy conventions

--- src/analyze.py
+++ src/analyze.py
@@ -1,9 +1,11 @@
-from load_data import load_data
 import argparse
+from load_data import load_data
 
 
-parser = argparse.ArgumentParser(description='Slow dancing with wind turbines.')
-parser.add_argument('data_path', help='path to the directory in which the data is located.')
+parser = argparse.ArgumentParser(description='Slow dancing with wind ' \
+	+ 'turbines.')
+parser.add_argument('data_path', help='path to the directory in which ' \
+	+ 'the data is located.')
 parser.add_argument('--type', help='type of data to load (ARD or CAU).')
 
 data = load_data(data_path, type)
--- src/load_data.py
+++ src/load_data.py
@@ -1,36 +1,36 @@
-import pandas as pd
 import os
+import pandas as pd
 
 
-def load_data(path: str, data_type: str, flag: bool = False):
+def load_data(path, data_type, flag=False):
     """
     Load wind turbine data.
     
     Parameters
     ----------
     path : str
-        the path to the directory in which the data is located.
-    data_type : 'ARD' or 'CAU'
-        which data to load.
-    flag : bool, default = False
-        whether to apply the 'normal operation' flag.
+        The path to the directory in which the data is located.
+    data_type : str
+        Which data to load, either 'ARD' or 'CAU'.
+    flag : bool, default=False
+        Whether to apply the 'normal operation' flag.
 
     Returns
     -------
     pd.DataFrame
-        loaded CSV data.
+        Loaded CSV data.
 
     Throws
     ------
     TypeError
-        if data_type is not 'ARD' or 'CAU'
+        If data_type is not 'ARD' or 'CAU'.
     """
     if data_type not in ('ARD', 'CAU'):
-        raise TypeError('Argument \'data_type\' must be \'ARD\' or \'CAU\'.')
+        raise TypeError('Argument \'data_type\' must be \'ARD\' or ' \
+						+ '\'CAU\'.')
 
     data_file = data_type + '_Data.csv'
     data = pd.read_csv(os.path.join(path, data_file))
-
     assert type(data) is pd.DataFrame
     
     if flag:
@@ -38,105 +38,133 @@ def load_data(path: str, data_type: str, flag: bool = False):
         normal_operation = pd.read_csv(os.path.join(path, flag_file))
         joined_data = data.join(normal_operation, lsuffix='', rsuffix='f')
         queried_data = joined_data.query('value == 1')
-        queried_data.reset_index(drop=True,inplace=True) # Added as removing the flagged data messes up the indexing of the data
+		# Added as removing the flagged data messes up the data indexing
+        queried_data.reset_index(drop=True, inplace=True)
         assert type(queried_data) is pd.DataFrame
-
         return queried_data
 
     return data
 
 
-def load_positions(path: str, data_type: str, flag: bool = False):
+def load_positions(path, data_type, flag=False):
     """
     Load wind turbine relative position data.
     
     Parameters
     ----------
     path : str
-        the path to the directory in which the data is located.
-    type : 'ARD' or 'CAU'
-        which data to load.
+        The path to the directory in which the data is located.
+    data_type : str
+        Which data to load, either 'ARD' or 'CAU'.
+    flag : bool, default=False
+        Whether to apply the 'normal operation' flag.
 
     Returns
     -------
     pd.DataFrame
-        loaded xlsx data.
+        Loaded xlsx data.
 
     Throws
     ------
     ValueError
-        if type is not 'ARD' or 'CAU'
+        If type is not 'ARD' or 'CAU'.
     """    
     if data_type not in ('ARD', 'CAU'):
-        raise ValueError('Argument \'data_type\' must be \'ARD\' or \'CAU\'.')
+        raise ValueError('Argument \'data_type\' must be \'ARD\' or ' \ 
+						 + '\'CAU\'.')
+
     data_file = data_type + '_Turbine_Positions.csv'
     data = pd.read_csv(os.path.join(path, data_file))
-
     assert type(data) is pd.DataFrame
+
     if flag:
         flag_file = data_type + '_Flag.csv'
         normal_operation = pd.read_csv(os.path.join(path, flag_file))
         joined_data = data.join(normal_operation, lsuffix='', rsuffix='f')
-        joined_data.reset_index(drop=True,inplace=True) # Added as removing the flagged data messes up the indexing of the data
+		# Added as removing the flagged data messes up the data indexing
+        joined_data.reset_index(drop=True,inplace=True)
         return joined_data.query('value == 1')
 
     return data
 
 
-def load_data_positions(path: str, data_type: str, flag: bool = False):
+def load_data_positions(path, data_type, flag=False):
     """
-    Load wind turbine data and relative position data, combine them as one dataframe
+    Load wind turbine data and relative position data combined.
     
     Parameters
     ----------
     path : str
-        the path to the directory in which the data is located.
-    type : 'ARD' or 'CAU'
-        which data to load.
+        The path to the directory in which the data is located.
+    type : str
+        Which data to load, either 'ARD' or 'CAU'.
 
     Returns
     -------
     pd.DataFrame
-        loaded xlsx data.
+        Loaded xlsx data.
 
     Throws
     ------
     TypeError
-        if type is not 'ARD' or 'CAU'
+        If type is not 'ARD' or 'CAU'.
     """    
 
     if data_type not in ('ARD', 'CAU'):
-        raise TypeError('Argument \'data_type\' must be \'ARD\' or \'CAU\'.')
+        raise TypeError('Argument \'data_type\' must be \'ARD\' or ' \
+						+ '\'CAU\'.')
     
     data_file = data_type + '_Data.csv'
     data = pd.read_csv(os.path.join(path, data_file))
+    assert type(data) is pd.DataFrame
 
     pos_file = data_type + '_Turbine_Positions.csv'
     pos = pd.read_csv(os.path.join(path, pos_file))
 
-    assert type(data) is pd.DataFrame
     if flag:
         flag_file = data_type + '_Flag.csv'
         normal_operation = pd.read_csv(os.path.join(path, flag_file))
         data = data.join(normal_operation, lsuffix='', rsuffix='f')
-        data.reset_index(drop=True,inplace=True) # Added as removing the flagged data messes up the indexing of the data
+		# Added as removing the flagged data messes up the data indexing
+        data.reset_index(drop=True,inplace=True)
 
-    data_joined = pd.merge(data,pos,left_on=["instanceID"],right_on=["Obstical"])
-    #data_joined.reset_index(drop=True,inplace=True)
+    data_joined = pd.merge(data, pos, left_on=["instanceID"], \
+						   right_on=["Obstical"])
+    # data_joined.reset_index(drop=True,inplace=True)
     return data_joined
     
-def select_time(data,time,verbose=False):
+def select_time(data, time, verbose=False):
     """
-    Returns all wind turbines at a given time
-    """
-    if verbose:
-        print("Selected time: "+str(data.ts[time]))
-    return data[data.ts==data.ts[time]]
+    Return the data for all wind turbines at the specified time.
 
-def select_turbine(data,turbine,verbose=False):
-    """
-    Returns one wind turbine's data across all times
+	Parameters
+	----------
+	data : pd.DataFrame
+	time : str
+	verbose : bool, default=False
+
+	Returns
+	-------
+	pd.Series
     """
     if verbose:
-        print("Selected turbine "+str(data.instanceID[turbine]))
-    return data[data.instanceID==data.instanceID[turbine]]
+        print("Selected time: " + str(data.ts[time]))
+    return data[data.ts == data.ts[time]]
+
+def select_turbine(data, turbine, verbose=False):
+    """
+    Return the data for one wind turbine across all times.
+
+	Parameters
+	----------
+	data : pd.DataFrame
+	time : str
+	verbose : bool, default=False
+
+	Returns
+	-------
+	pd.Series
+    """
+    if verbose:
+        print("Selected turbine " + str(data.instanceID[turbine]))
+    return data[data.instanceID == data.instanceID[turbine]]
