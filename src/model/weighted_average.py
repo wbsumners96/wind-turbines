ffed2c8637bfda5b4823ea25cb47954af32c0d39
Alex <alexander.richardson@gmail.com>
2022-01-26 11:30:12 +0000

    added tensor version of weighted_average, as well as updating the predict() function such that it checks what datatype the TurbineData is storing and runs the appropriate method

--- src/model/weighted_average.py
+++ src/model/weighted_average.py
@@ -7,10 +7,84 @@ class WeightedAverage(Predictor):
     def __init__(self, weighting):
         self.weighting = weighting
 
-    def predict(self, data, targets, references, times):
+
+
+    def predict(self,data,targets,references,times=None):
+        """
+        Calls predict_tensor or predict_pd depending on datatype
+        """
+        if data.datatype=="pd.DataFrame":
+            return predict_pd(data,targets,references,times)
+        if data.datatype=="np.ndarray":
+            return predict_tensor(data,targets,references)
+
+    def predict_tensor(self,data,tar_mask,ref_mask,verbose=False):
+        """
+        Predict the power of the specified wind turbines.
+        Needs data as a numpy array, parallel over time axis
+        
+
+        Parameters
+        ----------
+        data : numpy.ndarray
+            Wind turbine data.
+        targets : list of int
+            ID of target turbine.
+        references : list of int
+            IDs of reference turbines.
+        verbose : bool
+            Choose whether to display heatmap of weight matrix
+
+        Returns
+        -------
+        pred_power : numpy.ndarray
+            Estimated power output of the target turbines.
+        tar_power : numpy.ndarray
+            Measured power output of the target turbines.
+        """
+
+        if data.datatype!='np.ndarray':
+            raise TypeError('Data must be numpy array, run .to_tensor() first')
+        data = data.data
+
+        tars = data[:,tar_mask]
+        refs = data[:,ref_mask]
+
+        if not np.all(tars[:,-1]):
+            print("Warning: some target turbines are faulty")
+        if not np.all(refs[:,-1]):
+            print("Warning: some reference turbines are faulty")
+
+        # Position data
+        tar_pos = tars[0,:,5:6] # turbines don't move
+        ref_pos = refs[0,:,5:6]
+        # Power data
+        tar_power = tars[:,:,2]
+        ref_power = refs[:,:,2]
+
+        # Calculate euclidean distance between all target-reference pairs
+        ds = np.sqrt(np.sum((tar_pos[:,np.newaxis,:]-ref_pos)**2,axis=-1))
+
+        ws = np.vectorize(weighting)(ds)
+        if verbose:
+            plt.imshow(ws)
+            plt.title('Weight matrix')
+            plt.show()
+        
+
+        def f(power):
+            # Dummy function to change later if we want something more complex 
+            return power
+
+        vf = np.vectorize(f)
+        pred_power = np.einsum('ij, kj->ki', ws, ref_power)/np.sum(ws, axis=1)
+        
+        return pred_power, tar_power
+
+    def predict_pd(self, data, targets, references, times):
         """
         Predict the power of the target turbine at the specified time.
-
+    
         The power is predicted by taking a weighted average of the powers of the
         given reference turbines at that time. The coefficients in the weighted
         average are given by a function of distance from the target turbine.
@@ -43,6 +117,9 @@ class WeightedAverage(Predictor):
         """
         # Generate string IDs of turbines
         # First learn the type of the data
+        if data.datatype!='pd.DataFrame':
+            raise TypeError('Data must be pandas dataframe')
+        data = data.data
         first_id = data['instanceID'][0]
         if first_id.startswith('ARD'):
             type = 'ARD'
