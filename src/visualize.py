6bba93798f2a83d748f9de87c63330984a731d8a
Billy Sumners <w.b.sumners@sms.ed.ac.uk>
2022-03-09 15:16:57 +0000

    Modify formatting.

--- src/visualize.py
+++ src/visualize.py
@@ -79,239 +79,197 @@ def direction_power_histogram(data):
 	----------
 	data : pd.DataFrame
 	"""
-	
-
-	if data.data_type=="np.ndarray":
-		_dir = data.data[:,:,4].reshape(-1)
-		_pow = data.data[:,:,2].reshape(-1)
-		data_np = np.stack((_dir,_pow),axis=-1)
-		print(data_np.shape)
-	if data.data_type=="pd.DataFrame":
+	if data.data_type == 'np.ndarray':
+		_dir = data.data[:, :, 4].reshape(-1)
+		_pow = data.data[:, :, 2].reshape(-1)
+		data_np = np.stack((_dir, _pow), axis=-1)
+	elif data.data_type == 'pd.DataFrame':
 		data_np = data.data[['Wind_direction_calibrated', 'Power']].to_numpy()
-		print(data_np.shape)
 
-	n_bins = 100
-	
-	#print(np.nanmax(data_np[:,0]))
-	
-	plt.hist2d(data_np[:,0],data_np[:,1],bins=100,cmap="inferno")
-	plt.xlabel("Turbine angle (degrees)")
-	plt.ylabel("Turbine power (kW)")
+	plt.hist2d(data_np[:, 0], data_np[:, 1], bins=100, cmap='inferno')
+	plt.xlabel('Turbine angle (degrees)')
+	plt.ylabel('Turbine power (kW)')
 	plt.title(data.farm)
 	plt.colorbar()
 	plt.show()
 
-	
-	"""
-	hist, xedges, yedges = np.histogram2d(
-						 data_np[:, 1], data_np[:, 0], bins=n_bins,
-						 range=[[0, np.nanmax(data_np[:, 1])],[0, 360]])
-	#print(xedges.shape)
 
-	fig,ax = plt.subplots(1,1,figsize=(10,8))
-	img = ax.imshow(hist,origin="lower")
-	ax.set_xticks(np.arange(n_bins+1)[::10])
-	ax.set_yticks(np.arange(n_bins+1)[::10])
-	ax.set_xticklabels(yedges[::10].astype(int))
-	ax.set_yticklabels(xedges[::10].astype(int))
-	ax.set_title("ARD")
-	ax.set_xlabel("Turbine angle (degrees)")
-	ax.set_ylabel("Power output (kW)")
-	#ax.yaxis.set_major_formatter(mpl.ticker.FormatStrFormatter('%.2f'))
-	ax.tick_params(axis="x",labelrotation=-90)
-	fig.colorbar(img)
-	plt.show()
-	"""
-
-def prediction_measured_histogram(predictions,measurements):
-	#plt.scatter(measurements,predictions,alpha=0.4)
-	plt.hist2d(measurements,predictions,bins=100,norm=mpl.colors.LogNorm(),cmap="binary")
-	plt.xlabel("Measured Power (kW)")
-	plt.ylabel("Predicted Power (kW)")
+def prediction_measured_histogram(predictions, measurements):
+	plt.hist2d(measurements, predictions, bins=100, norm=mpl.colors.LogNorm(), 
+            cmap='binary')
+	plt.xlabel('Measured Power (kW)')
+	plt.ylabel('Predicted Power (kW)')
 	plt.show()
 
 
+def visualize_cor_func_behaviour(X, Y, ys):
+	cmap='binary'
+	x = X[:, 0]
+	xa = X[:, 1]
+	y = Y[:, 0]
+	ya = Y[:, 1]
 
-
-def visualize_cor_func_behaviour(X,Y,ys):
-	cmap="binary"
-	x = X[:,0]
-	xa = X[:,1]
-	y = Y[:,0]
-	ya = Y[:,1]
-	print(y.shape)
-	print(ys[:,0].shape)
-	#measured vs predicted powers
-	plt.scatter(x[::10],y[::10],label="Measured",alpha=0.05)
-	plt.scatter(x[1::10],ys[1::10,0],label="Prediction (on test data)",alpha=0.05)
-	plt.xlabel("Reference power")
+	plt.scatter(x[::10], y[::10], label='Measured', alpha=0.05)
+	plt.scatter(x[1::10], ys[1::10, 0], label='Prediction (on test data)', 
+            alpha=0.05)
+	plt.xlabel('Reference power')
 	plt.legend()
-	plt.ylabel("Target power")
+	plt.ylabel('Target power')
 	plt.show()
 
-	plt.scatter(xa[::10],ya[::10],label="Measured",alpha=0.05)
-	plt.scatter(xa[1::10],ys[1::10,1],label="Prediction (on test data)",alpha=0.05)
+	plt.scatter(xa[::10], ya[::10], label='Measured', alpha=0.05)
+	plt.scatter(xa[1::10], ys[1::10, 1], label='Prediction (on test data)', 
+            alpha=0.05)
 
-	plt.xlabel("Reference angle")
+	plt.xlabel('Reference angle')
 	plt.legend()
-	plt.ylabel("Target angle")
+	plt.ylabel('Target angle')
 	plt.show()
 
+	_, ax = plt.subplots(2, 1, sharex=True, sharey=True)
 
+	ax[0].hist2d(xa, xa-ya, bins=100,
+            range=[[np.min(xa), np.max(xa)], [-20, 20]],
+            norm=mpl.colors.LogNorm(), cmap=cmap)
 
+	ax[0].set_ylabel(r'$\theta_j-\theta_i$')
+	ax[0].set_title('Measured')
 
-	fig,ax = plt.subplots(2,1,sharex=True,sharey=True)
+	ax[1].hist2d(xa, xa-ys[:, 1], bins=100,
+            range=[[np.min(xa), np.max(xa)], [-20, 20]],
+            norm=mpl.colors.LogNorm(), cmap=cmap)
+	ax[1].set_xlabel(r'$\theta_j$')
+	ax[1].set_ylabel(r'$\theta_j - \hat{\theta}_i$')
+	ax[1].set_title('Predicted')
 
-	ax[0].hist2d(xa,xa-ya,bins=100,range=[[np.min(xa),np.max(xa)],[-20,20]],norm=mpl.colors.LogNorm(),cmap=cmap)
-	#ax[0].set_xlabel("Reference angle")
-	ax[0].set_ylabel(r"$\theta_j-\theta_i$")
-	ax[0].set_title("Measured")
-
-	ax[1].hist2d(xa,xa-ys[:,1],bins=100,range=[[np.min(xa),np.max(xa)],[-20,20]],norm=mpl.colors.LogNorm(),cmap=cmap)
-	ax[1].set_xlabel(r"$\theta_j$")
-	ax[1].set_ylabel(r"$\theta_j - \hat{\theta}_i$")
-	ax[1].set_title("Predicted")
 	plt.show()
 
+	_, ax = plt.subplots(2, 1, sharex=True, sharey=True)
 
+	ax[0].hist2d(xa, (x-y), bins=100, norm=mpl.colors.LogNorm(), cmap=cmap)
+	ax[0].set_ylabel(r'$P_i-P_j$')
+	ax[0].set_title('Measured')
 
-
-	fig,ax = plt.subplots(2,1,sharex=True,sharey=True)
-	ax[0].hist2d(xa,(x-y),bins=100,norm=mpl.colors.LogNorm(),cmap=cmap)
-	#ax[0].set_xlabel("Average angle of target/reference pair")
-	ax[0].set_ylabel(r"$P_i-P_j$")
-	ax[0].set_title("Measured")
-
-	ax[1].hist2d(xa,(x-ys[:,0]),norm=mpl.colors.LogNorm(),bins=100,cmap=cmap)
-	ax[1].set_xlabel(r"$\theta_j$")
-	#ax[1].set_xlabel(r"$\frac{\theta_i+\theta_j}{2}$")
-	ax[1].set_ylabel(r"$P_i-\hat{P}_j$")
-	ax[1].set_title("Predicted")
-	#plt.title("Power difference vs mean angle correlation")
+	ax[1].hist2d(xa, (x-ys[:, 0]), norm=mpl.colors.LogNorm(), bins=100, 
+            cmap=cmap)
+	ax[1].set_xlabel(r'$\theta_j$')
+	ax[1].set_ylabel(r'$P_i-\hat{P}_j$')
+	ax[1].set_title('Predicted')
 	plt.show()
 
-
-	plt.hist2d(ya,y-ys[:,0],bins=100,norm=mpl.colors.LogNorm(),cmap=cmap)
-	plt.xlabel(r"$\theta_j$")
-	plt.ylabel(r"$P_j - \hat{P}_j$")
+	plt.hist2d(ya, y-ys[:, 0], bins=100, norm=mpl.colors.LogNorm(), cmap=cmap)
+	plt.xlabel(r'$\theta_j$')
+	plt.ylabel(r'$P_j - \hat{P}_j$')
 	plt.show()
 
-
-
-
-
-
-	plt.scatter((xa[::10]+ya[::10])/2,(x[::10]-y[::10]),label="Measured",alpha=0.05)
-	plt.scatter((xa[::10]+ys[::10,1])/2,(x[::10]-ys[::10,0]),label="Prediction",alpha=0.05)
-	plt.xlabel(r"$\frac{\theta_i+\theta_j}{2}$")
-	plt.ylabel(r"$P_i-P_j$")
-	plt.title("Power difference vs mean angle correlation")
+	plt.scatter((xa[::10] + ya[::10])/2, (x[::10] - y[::10]), label='Measured', 
+            alpha=0.05)
+	plt.scatter((xa[::10] + ys[::10, 1])/2, (x[::10] - ys[::10,0]), 
+            label='Prediction', alpha=0.05)
+	plt.xlabel(r'$\frac{\theta_i+\theta_j}{2}$')
+	plt.ylabel(r'$P_i-P_j$')
+	plt.title('Power difference vs mean angle correlation')
 	plt.legend()
 	plt.show()
 
-	plt.hist2d(y,(ys[:,0]),bins=100,norm=mpl.colors.LogNorm(),cmap=cmap)
-	plt.xlabel(r"$P_j$")
-	plt.ylabel(r"$\hat{P}_j$")
+	plt.hist2d(y, ys[:, 0], bins=100, norm=mpl.colors.LogNorm(), cmap=cmap)
+	plt.xlabel(r'$P_j$')
+	plt.ylabel(r'$\hat{P}_j$')
 	plt.show()
 
-
-
-	plt.scatter(y,(y-ys[:,0])/y,alpha=0.1)
-	plt.xlabel(r"$P_j$")
-	plt.ylabel(r"$\frac{P_j - \hat{P}_j}{P_j}$")
+	plt.scatter(y, (y - ys[:, 0])/y, alpha=0.1)
+	plt.xlabel(r'$P_j$')
+	plt.ylabel(r'$\frac{P_j - \hat{P}_j}{P_j}$')
 	plt.show()
 
-
-	plt.hist2d(y,(y-ys[:,0])/(1+y),bins=100,norm=mpl.colors.LogNorm(),cmap=cmap)
-	plt.xlabel(r"$P_j$")
-	plt.ylabel(r"$\frac{P_j - \hat{P}_j}{P_j}$")
+	plt.hist2d(y, (y - ys[:, 0])/(1 + y), bins=100, norm=mpl.colors.LogNorm(), 
+            cmap=cmap)
+	plt.xlabel(r'$P_j$')
+	plt.ylabel(r'$\frac{P_j - \hat{P}_j}{P_j}$')
 	plt.show()
 
-	h,xedges,yedges=np.histogram2d(y,(y-ys[:,0]),bins=50)
-	print(yedges)
-	print(h)
-	plt.imshow(h.T,origin="lower",
-				   extent=[xedges[0],xedges[-1],yedges[0],yedges[-1]],
-				   interpolation="gaussian",
-				   cmap="Greens")
-	h_av = np.zeros((xedges.shape[0]-1))
-	h_var = np.zeros((xedges.shape[0]-1))
+	h, xedges, yedges = np.histogram2d(y, y - ys[:, 0], bins=50)
+	plt.imshow(h.T, origin='lower',
+            extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]], 
+            interpolation='gaussian', cmap='Greens')
+
+	h_av = np.zeros(xedges.shape[0] - 1)
+	h_var = np.zeros(xedges.shape[0] - 1)
 	
 	for i in range(h_av.shape[0]):
-		h_av[i] = np.average(yedges[1:],weights=h[i])
-		h_var[i]= np.sqrt(np.average((yedges[1:]-h_av[i])**2,weights=h[i]))
-	plt.plot(xedges[:-1],h_av,color="red")
-	plt.plot(xedges[:-1],h_av+h_var,color="red",alpha=0.2)
-	plt.plot(xedges[:-1],h_av-h_var,color="red",alpha=0.2)
+		h_av[i] = np.average(yedges[1:], weights=h[i])
+		h_var[i]= np.sqrt(np.average((yedges[1:] - h_av[i])**2, weights=h[i]))
+
+	plt.plot(xedges[:-1], h_av, color='red')
+	plt.plot(xedges[:-1], h_av + h_var, color='red', alpha=0.2)
+	plt.plot(xedges[:-1], h_av - h_var, color='red', alpha=0.2)
 	plt.show()
 
-	plt.hist(y,bins=100,alpha=0.4,label="Measured",density=True)
-	plt.hist(ys[:,0],bins=100,alpha=0.4,label="Predicted",density=True)
-	plt.xlabel("Target turbine power (kW)")
+	plt.hist(y, bins=100, alpha=0.4, label='Measured', density=True)
+	plt.hist(ys[:, 0], bins=100, alpha=0.4, label='Predicted', density=True)
+	plt.xlabel('Target turbine power (kW)')
 	plt.legend()
-	plt.ylabel("Count")
+	plt.ylabel('Count')
 	plt.show()
 
 
-def average_power_gain_curve(data,k_mat):
-	def all_predictions(data,k_mat):
-		N=data.n_turbines
-		#N=2
+def average_power_gain_curve(data, k_mat):
+	def all_predictions(data, k_mat):
+		N = data.n_turbines
 		its = list(range(N))
-		predictions = [[] for n in range(N)]
-		measurements = [[] for n in range(N)]
-		errors = [[] for n in range(N)]
+		predictions = [[] for _ in range(N)]
+		measurements = [[] for _ in range(N)]
+		errors = [[] for _ in range(N)]
 		for i in tqdm(its):
-			for j in tqdm(its[:i]+its[i+1:]):
+			for j in tqdm(its[:i] + its[i + 1:]):
 				data_copy = copy.deepcopy(data)
-				data_copy.select_turbine([i,j])
+				data_copy.select_turbine([i, j])
 				data_copy.select_normal_operation_times()
 				data_copy.select_unsaturated_times()
 				data_copy.select_power_min()
-				x = data_copy.data[:,1,2] # reference power
-				xa = data_copy.data[:,1,4] # reference angle
-				y = data_copy.data[:,0,2] # target power
-				#ya = data_copy.data[:,0,4] # target angle
-				X = np.stack((x,xa),axis=1)
-				#Y = np.stack((y,ya),axis=1)
+				x = data_copy.data[:, 1, 2] # reference power
+				xa = data_copy.data[:, 1, 4] # reference angle
+				y = data_copy.data[:, 0, 2] # target power
+				X = np.stack((x, xa), axis=1)
 
-				#print("Available datapoints: "+str(x.shape[0]))
-				p = k_mat[i,j].predict(X)
-				predictions[i].append(p[:,0])
+				p = k_mat[i, j].predict(X)
+				predictions[i].append(p[:, 0])
 				measurements[i].append(y)
-				errors[i].append(y-p[:,0])
-		print(predictions)
-		return predictions,measurements,errors
+				errors[i].append(y - p[:, 0])
 
-	_,measured,errors=all_predictions(data,k_mat)
-	#Plots power errors as functions of measured powers, averaged over turbines, like Oli showed in meeting
-	M = np.array(measured,dtype=object).flatten()
-	E = np.array(errors,dtype=object).flatten()
+		return predictions, measurements, errors
+
+	_, measured, errors = all_predictions(data, k_mat)
+
+	# Plots power errors as functions of measured powers, averaged over
+    # turbines, like Oli showed in meeting
+	M = np.array(measured, dtype=object).flatten()
+	E = np.array(errors, dtype=object).flatten()
 
 	M = np.array(M,dtype=float)
 	E = np.array(E,dtype=float)
-	#P = predicted.reshape(-1)
-	print(E)
-	print(M)
 
-	h,xedges,yedges=np.histogram2d(M,E,bins=100,range=[[M.min(),M.max()],[E.min(),E.max()]])
-	print(yedges)
-	print(h)
-	plt.imshow(h.T,origin="lower",
-				   extent=[xedges[0],xedges[-1],yedges[0],yedges[-1]],
-				   interpolation="gaussian",
-				   cmap="Greens")
-	h_av = np.zeros((xedges.shape[0]-1))
-	h_var = np.zeros((xedges.shape[0]-1))
+	h, xedges, yedges = np.histogram2d(M, E, bins=100, 
+            range=[[M.min(), M.max()], [E.min(), E.max()]])
+
+	plt.imshow(h.T, origin='lower', 
+            extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]], 
+            interpolation='gaussian', cmap='Greens')
+
+	h_av = np.zeros(xedges.shape[0] - 1)
+	h_var = np.zeros(xedges.shape[0] - 1)
 	
 	for i in range(h_av.shape[0]):
-		h_av[i] = np.average(yedges[1:],weights=h[i])
-		h_var[i]= np.sqrt(np.average((yedges[1:]-h_av[i])**2,weights=h[i]))
-	plt.plot(xedges[:-1],h_av,color="red",label="Weighted average")
-	plt.plot(xedges[:-1],h_av+h_var,color="red",alpha=0.2,label="Standard deviation")
-	plt.plot(xedges[:-1],h_av-h_var,color="red",alpha=0.2)
-	plt.xlabel("$P$")
-	plt.ylabel("$\delta P$")
+		h_av[i] = np.average(yedges[1:], weights=h[i])
+		h_var[i] = np.sqrt(np.average((yedges[1:] - h_av[i])**2, weights=h[i]))
+
+	plt.plot(xedges[:-1], h_av, color='red', label='Weighted average')
+	plt.plot(xedges[:-1], h_av + h_var, color='red', alpha=0.2, 
+            label='Standard deviation')
+	plt.plot(xedges[:-1], h_av - h_var, color='red', alpha=0.2)
+	plt.xlabel(r'$P$')
+	plt.ylabel(r'$\delta P$')
 	plt.colorbar()
 	plt.legend()
 	plt.show()
+
