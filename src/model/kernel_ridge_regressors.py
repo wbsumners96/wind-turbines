6e9d9ef681cc5b1fb7d779c6df820919d2f404d4
Billy Sumners <w.b.sumners@sms.ed.ac.uk>
2022-03-16 21:59:20 +0000

    It works!!

--- src/model/kernel_ridge_regressors.py
+++ src/model/kernel_ridge_regressors.py
@@ -1,7 +1,9 @@
 from abc import abstractmethod
 from joblib import dump, load
 import numpy as np
+import os
 import pandas as pd
+from pathlib import Path
 from sklearn.compose import TransformedTargetRegressor
 from sklearn.kernel_ridge import KernelRidge
 from sklearn.model_selection import train_test_split
@@ -61,79 +63,101 @@ class KernelRidgeRegressor(Predictor):
         """
         raise NotImplementedError()
 
-    def predict(self, data, targets, references, times):
-        prediction_data = {'ts': [],
-                           'target_id': [],
-                           'reference_id': [],
-                           'target_power': [],
-                           'predicted_power': []}
-        for target_number in targets:
-            target_id = f'ARD_WTG{target_number:02}' 
-            target = data.select_turbine(target_id)
-            
-            target_data = target[['ts',
-                                  'instanceID', 'Power',
-                                  'Wind_direction_calibrated']]
-            target_data.rename({'instanceID': 'target_id',
+    def predict(self, data, targets, references, times=None):
+        target_ids = [f'ARD_WTG{target_number:02}' for target_number in targets]
+        reference_ids = [f'ARD_WTG{reference_number:02}' for reference_number 
+                in references]
+
+        targets = data.data.query('instanceID == @target_ids')
+        references = data.data.query('instanceID == @reference_ids')
+
+        targets = targets[['ts',
+                           'instanceID',
+                           'Power',
+                           'Wind_direction_calibrated']]
+        targets.rename(columns={'instanceID': 'target_id',
                                 'Power': 'target_power',
                                 'Wind_direction_calibrated': 'target_angle'},
-                               inplace=True)
+                       inplace=True)
 
-            target_regressors = load(f'regressors/' + \
-                    '{target_number}_kernel_ridge_regressors.joblib')
-            for reference_number in references:
-                reference_id = f'ARD_WTG{reference_number:02}'
-                reference = data.select_turbine(reference_id)
-                
-                reference_data = reference[['ts',
-                                            'instanceID', 
-                                            'Power',
-                                            'Wind_direction_calibrated']]
-                reference_data.rename({'instanceID': 'reference_id',
-                                       'Power': 'reference_power',
-                                       'Wind_direction_calibrated':
-                                               'reference_angle'},
-                                      inplace=True)
+        references = references[['ts',
+                                 'instanceID', 
+                                 'Power',
+                                 'Wind_direction_calibrated']]
+        references.rename(columns={'instanceID': 'reference_id',
+                                   'Power': 'reference_power',
+                                   'Wind_direction_calibrated': 
+                                           'reference_angle'},
+                          inplace=True)
 
-                merged_data = pd.merge(target_data, reference_data, on='ts')
-                ts = merged_data['ts'].to_numpy()
-                target_id = merged_data['target_id'].to_numpy()
-                reference_id = merged_data['reference_id'].to_numpy()
-                target_power = merged_data['target_power'].to_numpy()
-                reference_power = merged_data['reference_power'].to_numpy()
-                target_angle = merged_data['target_angle'].to_numpy()
-                reference_angle = merged_data['reference_angle'].to_numpy()
+        merged_data = pd.merge(targets, references, on='ts')
+        merged_data['predicted_power'] = 0
+        
+        for target_id in tqdm(target_ids, desc='Target', leave=False):
+            regressors_path = Path('~/.turbines/regressors/').expanduser() \
+                    / f'{target_id}_kernel_ridge_regressors.joblib'
+            regressors = load(regressors_path)
 
-                features = np.column_stack([reference_power,
-                                            target_angle,
-                                            reference_angle])
-                kernel_gram = self.kernel(features, self.features_train)
+            for reference_id in tqdm(reference_ids, desc='Reference',
+                    leave=False):
+                if target_id == reference_id:
+                    predictions = merged_data['target_power']
+                else:
+                    select_data = merged_data.query('target_id == @target_id '+\
+                            'and reference_id == @reference_id')
 
-                regressor = target_regressors[reference_id]
-                predicted_power = regressor.predict(kernel_gram)
+                    reference_powers = select_data['reference_power'].to_numpy()
+                    target_angle = select_data['target_angle'].to_numpy()
+                    reference_angle = select_data['reference_angle'].to_numpy()
 
-                prediction_data['ts'].extend(ts)
-                prediction_data['target_id'].extend(target_id)
-                prediction_data['reference_id'].extend(reference_id)
-                prediction_data['target_power'].extend(target_power)
-                prediction_data['predicted_power'].extend(predicted_power)
+                    features = np.column_stack([reference_powers,
+                                                target_angle,
+                                                reference_angle])
+                    training_features = self.features_train[target_id] \
+                            [reference_id]
 
-        return pd.DataFrame(prediction_data)
+                    kernel_gram = self.kernel(training_features, features)
 
-    def __init__(self, data):
+                    regressor = regressors[reference_id]
+                    predictions = regressor.predict(kernel_gram)
+
+                mask = (merged_data['target_id'] == target_id) & \
+                       (merged_data['reference_id'] == reference_id)
+                merged_data.loc[mask, 'predicted_power'] = predictions
+
+        merged_data.drop(columns=['target_angle', 'reference_angle'],
+                         inplace=True)
+
+        return merged_data
+
+    def __init__(self, data=None):
+        features_train_path = \
+                Path('~/.turbines/training_features.joblib') \
+                        .expanduser()
+        if features_train_path.is_file():
+            self.features_train = load(features_train_path)
+        else:
+            self.fit(data)
+
+    def fit(self, data):
         """
         Fit a collection of kernel ridge models of the form target_power = 
         f(reference_power, target_angle, reference_angle) for each pair of target
         and reference turbines in the farm.
         """
-        target = data.select_turbine('ARD_WTG01')
-        target = target[['ts', 'instanceID', 'Power', 'Wind_direction_calibrated']]
-        target.rename(columns={'instanceID': 'target_id',
-                               'Power': 'target_power',
-                               'Wind_direction_calibrated': 'target_angle'},
-                      inplace=True)
-
         self.features_train = {}
+        turbines_dir = Path('~/.turbines').expanduser()
+        if not turbines_dir.is_dir():
+            os.mkdir(turbines_dir)
+
+        regressor_dir = Path('~/.turbines/regressors').expanduser()
+        if not regressor_dir.is_dir():
+            os.mkdir(regressor_dir) 
+
+        scores_dir = Path('~/.turbines/scores/').expanduser()
+        if not scores_dir.is_dir():
+            os.mkdir(scores_dir)
+
         for target_number in tqdm(range(1, 16), desc='Target'):
             target_id = f'ARD_WTG{target_number:02}'
             target = data.select_turbine(target_id)
@@ -144,7 +168,7 @@ class KernelRidgeRegressor(Predictor):
             target.rename(columns={'instanceID': 'target_id',
                                    'Power': 'target_power',
                                    'Wind_direction_calibrated': 'target_angle'},
-                             inplace=True)
+                          inplace=True)
 
             target_regressors = {}
             target_scores = {}
@@ -201,12 +225,21 @@ class KernelRidgeRegressor(Predictor):
                 target_scores[reference_id] = regressor.score(kernel_test,
                                                               label_test)
 
-            dump(target_regressors,
-                 f'regressors/{target_number}_kernel_ridge_regressors.joblib')
-            dump(target_scores, 
-                 f'scores/{target_number}_kernel_ridge_scores.joblib')
+            target_regressors_path = Path('~/.turbines/regressors/' + \
+                    f'{target_id}_kernel_ridge_regressors.joblib') \
+                    .expanduser()
+            target_scores_path = Path('~/.turbines/scores/' + \
+                    f'{target_id}_kernel_ridge_scores.joblib') \
+                    .expanduser()
+            dump(target_regressors, target_regressors_path)
+            dump(target_scores, target_scores_path)
 
             self.features_train[target_id] = target_features_train
+
+        training_features_path = Path('~/.turbines/' + \
+                'training_features.joblib') \
+                .expanduser()
+        dump(self.features_train, training_features_path)
 
 
 class LaplacianKRR(KernelRidgeRegressor):
